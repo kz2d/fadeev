89a33f1497b546dc7f2c4ecfba2bbbfcffb6473a
Arina Galieva <a.galieva@just-ai.com>
2022-09-01 02:37:13 +0300

    fixup! fixup! optimize js

--- scripts.js
+++ scripts.js
@@ -1,72 +1,38 @@
-function selectTabImplementation(node, tabsNode, newId) {
-    const newTab = tabsNode.querySelector(`.section__tab[data-id=${newId}]`);
-    const oldTab = tabsNode.querySelector('.section__tab_active');
-    const newPanel = node.querySelector(`.section__panel[data-id=${newId}]`);
-    const oldPanel = node.querySelector('.section__panel:not(.section__panel_hidden)');
-
-    oldTab.classList.remove('section__tab_active');
-    oldTab.setAttribute('aria-selected', 'false');
-    oldTab.removeAttribute('tabindex');
-    newTab.classList.add('section__tab_active');
-    newTab.setAttribute('aria-selected', 'true');
-    newTab.setAttribute('tabindex', '0');
-    newTab.focus({
-        preventScroll: true
-    });
-
-    oldPanel.classList.add('section__panel_hidden');
-    oldPanel.setAttribute('aria-hidden', 'true');
-    newPanel.classList.remove('section__panel_hidden');
-    newPanel.setAttribute('aria-hidden', 'false');
-}
-
-function onKeyDown(event, list, selected, selectTab, select) {
-    if (!event.target.classList.contains('section__tab') || event.ctrlKey || event.metaKey || event.shiftKey || event.altKey) {
-        return;
-    }
-
-    let index = list.indexOf(selected);
-    if (event.which === 37) {
-        // left
-        --index;
-    } else if (event.which === 39) {
-        // right
-        ++index;
-    } else if (event.which === 36) {
-        // home
-        index = 0;
-    } else if (event.which === 35) {
-        // end
-        index = list.length - 1;
-    } else {
-        return;
-    }
-
-    if (index >= list.length) {
-        index = 0;
-    } else if (index < 0) {
-        index = list.length - 1;
-    }
-
-    selectTab(list[index], select);
-}
-
 function makeTabs(node) {
     const tabsContainer = node.querySelector('.section__tabs');
-    const tabs = tabsContainer.querySelectorAll('.section__tab');
-    const datasets = Array.from(tabs).reduce((acc, node) => {
-        acc.ids.push(node.dataset.id);
-        if (node.classList.contains('.section__tab_active')) {
-            acc.activeId = node.dataset.id;
-        }
-        return acc;
-    }, {ids: [], activeId: ''});
     const select = node.querySelector('.section__select');
-    let selected = datasets.activeId;
+    const tabs = tabsContainer.querySelectorAll('.section__tab');
+    const list = [];
+    let selected = '';
+
+    tabs.forEach((node) => {
+        list.push(node.dataset.id);
+        if (node.classList.contains('.section__tab_active')) {
+            selected = node.dataset.id;
+        }
+    });
 
     function selectTab(newId) {
         selected = newId;
-        selectTabImplementation(node, tabsContainer, newId);
+        const newTab = tabsContainer.querySelector(`.section__tab[data-id=${newId}]`);
+        const oldTab = tabsContainer.querySelector('.section__tab_active');
+        const newPanel = node.querySelector(`.section__panel[data-id=${newId}]`);
+        const oldPanel = node.querySelector('.section__panel:not(.section__panel_hidden)');
+
+        oldTab.classList.remove('section__tab_active');
+        oldTab.setAttribute('aria-selected', 'false');
+        oldTab.removeAttribute('tabindex');
+        newTab.classList.add('section__tab_active');
+        newTab.setAttribute('aria-selected', 'true');
+        newTab.setAttribute('tabindex', '0');
+        newTab.focus({
+            preventScroll: true
+        });
+
+        oldPanel.classList.add('section__panel_hidden');
+        oldPanel.setAttribute('aria-hidden', 'true');
+        newPanel.classList.remove('section__panel_hidden');
+        newPanel.setAttribute('aria-hidden', 'false');
     }
 
     select.addEventListener('input', () => {
@@ -80,7 +46,28 @@ function makeTabs(node) {
     })
 
     tabsContainer.addEventListener('keydown', (event) => {
-        onKeyDown(event, datasets.ids, selected, selectTab, select);
+        if (!event.target.classList.contains('section__tab') || event.ctrlKey || event.metaKey || event.shiftKey || event.altKey) {
+            return;
+        }
+
+        let index = list.indexOf(selected);
+        if (event.keyCode === 37) {
+            if (--index < 0){
+                index = list.length - 1;
+            }
+        } else if (event.keyCode === 39) {
+            if (++index >= list.length) {
+                index = 0;
+            }
+        } else if (event.keyCode === 36) {
+            index = 0;
+        } else if (event.keyCode === 35) {
+            index = list.length - 1;
+        } else {
+            return;
+        }
+
+        selectTab(list[index], select);
     })
 }
 
