21abe41e49ace5dfec1f944ecfcca620921f3cd0
kz2d <kz2d@yandex-team.ru>
2022-09-01 13:07:35 +0300

    lol

--- scripts.js
+++ scripts.js
@@ -3,20 +3,20 @@ document.querySelectorAll('.main__devices').forEach(node => {
     const select = node.querySelector('.section__select');
     const tabs = tabsContainer.querySelectorAll('.section__tab');
 
-    const list = [];
+  const list = [];
     let selected = '';
-    tabs.forEach((node) => {
-        list.push(node.dataset.id);
+  tabs.forEach((node) => {
+    list.push(node.dataset.id);
         if (node.classList.contains('.section__tab_active')) {
-            selected = node.dataset.id;
-        }
-    });
+      selected = node.dataset.id;
+    }
+  });
 
-    function selectTab(newId) {
-        selected = newId;
+  function selectTab(newId) {
+    selected = newId;
         const newTab = tabsContainer.querySelector(`.section__tab[data-id=${newId}]`);
         const oldTab = tabsContainer.querySelector('.section__tab_active');
-        const newPanel = node.querySelector(`.section__panel[data-id=${newId}]`);
+    const newPanel = node.querySelector(`.section__panel[data-id=${newId}]`);
         const oldPanel = node.querySelector('.section__panel:not(.section__panel_hidden)');
 
         oldTab.classList.remove('section__tab_active');
@@ -25,62 +25,60 @@ document.querySelectorAll('.main__devices').forEach(node => {
         newTab.classList.add('section__tab_active');
         newTab.setAttribute('aria-selected', 'true');
         newTab.setAttribute('tabindex', '0');
-        newTab.focus({
+    newTab.focus({
             preventScroll: true
-        });
+    });
 
         oldPanel.classList.add('section__panel_hidden');
         oldPanel.setAttribute('aria-hidden', 'true');
         newPanel.classList.remove('section__panel_hidden');
         newPanel.setAttribute('aria-hidden', 'false');
-    }
+  }
 
     select.addEventListener('input', () => {
-        selectTab(select.value);
-    });
+    selectTab(select.value);
+  });
 
     tabsContainer.addEventListener('click', (event) => {
         if (event.target.classList.contains('section__tab')) {
-            selectTab(event.target.dataset.id);
-        }
+      selectTab(event.target.dataset.id);
+    }
     })
 
     tabsContainer.addEventListener('keydown', (event) => {
         if (!event.target.classList.contains('section__tab') || event.ctrlKey || event.metaKey || event.shiftKey || event.altKey) {
-            return;
-        }
+      return;
+    }
 
-        let index = list.indexOf(selected);
-        if (event.keyCode === 37) {
+    let index = list.indexOf(selected);
+    if (event.keyCode === 37) {
             if (--index < 0){
-                index = list.length - 1;
-            }
-        } else if (event.keyCode === 39) {
-            if (++index >= list.length) {
-                index = 0;
-            }
-        } else if (event.keyCode === 36) {
-            index = 0;
-        } else if (event.keyCode === 35) {
-            index = list.length - 1;
-        } else {
-            return;
-        }
+        index = list.length - 1;
+      }
+    } else if (event.keyCode === 39) {
+      if (++index >= list.length) {
+        index = 0;
+      }
+    } else if (event.keyCode === 36) {
+      index = 0;
+    } else if (event.keyCode === 35) {
+      index = list.length - 1;
+    } else {
+      return;
+    }
 
-        selectTab(list[index]);
-    })
-});
-document.querySelectorAll('.header__menu').forEach(node => {
-    let expanded = false;
-    const menuTextNode = node.querySelector('.header__menu-text');
-
-    node.addEventListener('click', () => {
-        expanded = !expanded;
-        node.setAttribute('aria-expanded', expanded ? 'true' : 'false');
-        menuTextNode.textContent = expanded ? 'Закрыть меню' : 'Открыть меню';
-        links.classList.toggle('header__links_opened', expanded);
-        links.classList.add('header__links-toggled');
-    });
+    selectTab(list[index]);
+  });
 });
 
-const links = document.querySelector('.header__links');
+  let expanded = false;
+function lol(node) {
+  const menuTextNode = node.querySelector(".header__menu-text");
+  expanded = !expanded;
+  node.setAttribute("aria-expanded", expanded ? "true" : "false");
+  menuTextNode.textContent = expanded ? "Закрыть меню" : "Открыть меню";
+  links.classList.toggle("header__links_opened", expanded);
+  links.classList.add("header__links-toggled");
+}
+
+const links = document.querySelector(".header__links");
